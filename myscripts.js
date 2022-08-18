8c85e4e80cb09bdb7283fecbc3823679cedf60d0
Creasser <blakecreasser@aggienetwork.com>
2022-08-18 17:05:41 -0500

    added all tie and win conditions to playround function

--- myscripts.js
+++ myscripts.js
@@ -11,7 +11,7 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
-
+//all the possible win and tie variations
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "Scissors") {
         return "Congrats! You Win!";
@@ -19,10 +19,28 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "Rock") {
         return "Tie Game!";
     }
+    if (playerSelection === "paper" && computerSelection === "Rock") {
+        return "Congrats! You Win!"
+    }
+    if (playerSelection === "paper" && computerSelection === "Paper") {
+        return "Tie Game!"
+    }
+    if (playerSelection === "scissors" && computerSelection === "Paper") {
+        return "Congrats! You Win!"
+    }
+    if (playerSelection === "scissors" && computerSelection === "Scissors") {
+        return "Tie Game!"
+    }
     else return "You Lose!"
 }
+//make another var to store the player selection, then convert that selection to lowercase and store it in the player selection var
+const playerChoice = prompt("Choose Rock, Paper, or Scissors");
+const playerSelection = playerChoice.toLowerCase();
+let computerSelection = getComputerChoice();
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
+
+
+    
+    
+    
