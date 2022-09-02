4e4526e55fcf9fabe15f755d271b12de6fdef7a2
Creasser <blakecreasser@aggienetwork.com>
2022-09-01 22:26:37 -0500

    added win conditions, added restart button, finished game

--- myscripts.js
+++ myscripts.js
@@ -10,13 +10,13 @@ function getComputerChoice() {
     const choice = ['Rock','Paper','Scissors'];
     let randChoice = Math.floor(Math.random() * choice.length) + 1;
     if (randChoice === 1) {
-        console.log("Computer chose rock")
+        //console.log("Computer chose rock")
         return "Rock";
    } if (randChoice === 2) {
-        console.log("Computer chose paper")
+        //console.log("Computer chose paper")
         return "Paper";
     } if (randChoice === 3) {
-        console.log("Computer chose scissors")
+       // console.log("Computer chose scissors")
         return "Scissors";
     }
 }
@@ -55,7 +55,7 @@ function playRound(userChoice, computerChoice) {
         content.textContent += 'You Lose! Paper beats Rock!'
         results.appendChild(content);
         computerScore++ ;
-
+        cs.textContent = computerScore;
     }
      if (userChoice === "rock" && computerChoice === "Scissors"){
         content.textContent = "Computer Chose Scissors. \r\n"
@@ -68,72 +68,79 @@ function playRound(userChoice, computerChoice) {
         content.textContent = "Computer Chose Rock. \r\n"
         content.textContent += 'Tie Game! Play again!'
         results.appendChild(content);
-        //console.log("Tie Game!");
      }
     //PAPER    
      if (userChoice === "paper" && computerChoice === "Rock"){ 
         content.textContent = "Computer Chose Rock. \r\n"
         content.textContent += 'You Win! Paper beats Rock!'
         results.appendChild(content);  
-        //console.log("You Win! Paper beats Rock!")
         playerScore++ ;
+        ps.textContent = playerScore
      }
      if (userChoice === "paper" && computerChoice === "Scissors"){
         content.textContent = "Computer Chose Scissors. \r\n"
         content.textContent += 'You Lose! Scissors beats Paper!'
         results.appendChild(content);
-        //console.log("You Lose! Scissors beats Paper!")
         computerScore++ ;
+        cs.textContent = computerScore
      }
      if (userChoice === "paper" && computerChoice === "Paper"){
         content.textContent = "Computer Chose Paper. \r\n"
         content.textContent += 'Tie Game! Play Again!'
         results.appendChild(content);
-        //console.log("Tie Game!");
      }
     //SCISSORS
      if (userChoice === "scissors" && computerChoice === "Rock"){
         content.textContent = "Computer Chose Rock. \r\n"
         content.textContent += 'You Lose! Rock beats Scissors!'
         results.appendChild(content);
-        //console.log("You Lose! Rock beats Scissors!")
         computerScore++ ; 
+        cs.textContent = computerScore;
      }
      if (userChoice === "scissors" && computerChoice === "Paper"){
         content.textContent = "Computer Chose Paper. \r\n"
         content.textContent += 'You Win! Scissors beats Paper!'
         results.appendChild(content);
-        //console.log("You Win! Scissors beats Paper!")
         playerScore++ ;
+        ps.textContent = playerScore
      }
      if (userChoice === "scissors" && computerChoice === "Scissors"){
         content.textContent = "Computer Chose Scissors. \r\n"
         content.textContent += 'Tie Game! Play Again!'
         results.appendChild(content);
-        //console.log("Tie Game!") 
      }   
-    
-            
-
-
+     winner()
+        function winner(){
+            if (playerScore === 5){
+                content.textContent = 'Congrats! You Won the Game!';
+                function disable(){
+                    window.playRound = function(){};
+                }
+                disable()
+            }
+            if (computerScore === 5){
+                content.textContent = 'You Suck! The Computer Won!';
+                function disable(){
+                    window.playRound = function(){};
+                }
+                disable()
+            }
+            else{
+                return
+            }
+        }   
 }    
-
 const ps = document.querySelector('.ps')
 const cs = document.querySelector('.cs')
-//let playerChoice = getPlayerChoice();
-
  const rock = document.querySelector('#rock')
     rock.addEventListener("click", function() {
         let computerChoice = getComputerChoice()
         playRound('rock', computerChoice)
-        //can possibly call on a function that will report the results and append the div in the center with the text
     })
-
 const paper = document.querySelector('#paper')
     paper.addEventListener('click', function(){
         let computerChoice = getComputerChoice()
         playRound('paper', computerChoice)
-        
     })
 
 const scissors = document.querySelector('#scissors')
@@ -142,7 +149,6 @@ const scissors = document.querySelector('#scissors')
         playRound('scissors', computerChoice)
     })
 
-//make a const for the div container, put the container.documenttextContent inside of the win conditions.
 
 
 
